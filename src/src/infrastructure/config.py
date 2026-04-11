1f88c4d806e737783e4a9dbac9d68d11ac1b5812
HariAakash646 <hariaakash646@gmail.com>
2026-04-11 13:28:32 +0530

    v0.1.56, Remove Mongo URI from infrastructure/config.py

--- src/src/infrastructure/config.py
+++ src/src/infrastructure/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    MONGO_URI: str = "mongodb+srv://nehaprabhup17_db_user:welcome12@dass.rt4ytmt.mongodb.net/chat_db?retryWrites=true&w=majority"
-    DB_NAME: str = "chat_db"
+    MONGO_URI: str
+    DB_NAME: str
     MODEL_NAME: str = "distilbert-base-cased-distilled-squad"
     EXTRACTION_MODEL_TYPE: str = "form_state"  # "gemma_functional" or "form_state"
     FORM_STATE_MODEL_PATH: str = "/app/data_generation/monomodel/model"   # Currently Qwen2.5-1.5b with LoRA
